9a2309a396d4b3448f00135c92f01420f951dcb0
JuanDavidGongora <145076953+JuanDavidGongora@users.noreply.github.com>
2023-11-12 23:44:08 -0500

    Update Habilities.jsx

--- src/components/Habilities.jsx
+++ src/components/Habilities.jsx
@@ -5,6 +5,8 @@ import javaScript from '../images/JavaScript.png'
 import react from '../images/React.png'
 import redux from '../images/Redux.png'
 import gitHub from '../images/Github.png'
+import reactRouter from '../images/reactRouter.png'
+
 
 const Habilities = () => {
     return (
@@ -38,6 +40,10 @@ const Habilities = () => {
                 <div className='tecInfo'>
                     <img src={gitHub} className='imgHabilities github'/>
                     <small>GitHub</small>
+                </div>
+                <div className='tecInfo'>
+                    <img src={reactRouter} className='imgHabilities'/>
+                    <small>React Router</small>
                 </div>
             </div>
             
