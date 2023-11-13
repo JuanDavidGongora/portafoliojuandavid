3c19f1c88038ec059e1f5ec2ab0b9d1cbc206eca
JuanDavidGongora <145076953+JuanDavidGongora@users.noreply.github.com>
2023-11-12 22:53:08 -0500

    Update Portfolio.jsx

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -8,6 +8,7 @@ import html from '../images/Html.png'
 import javaScript from '../images/JavaScript.png'
 import react from '../images/React.png'
 import redux from '../images/Redux.png'
+import reactRouter from '../images/reactRouter.png'
 
 
 import gitHub from '../images/Github.png'
